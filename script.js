74c74b6145ddfb623929a59c169756ff7783faed
Dakota Castle <barrettcastle@gmail.com>
2021-11-14 16:50:08 -0500

    adding game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Eventually this will be a little, fully-fleshed out game made using only what i learned at Flatiron!";
 
 document.querySelector("body").appendChild(h2);
